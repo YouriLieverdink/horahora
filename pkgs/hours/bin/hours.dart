6f7cd8d5b89ebdf75a8a5771bed2a59a79ac6108
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2023-08-11 14:26:30 +0200

    refactor: replace shelf_plus with dart_frog

--- pkgs/hours/bin/hours.dart
+++
@@ -1,4 +0,0 @@
-import 'package:hours/hours.dart';
-import 'package:shelf_plus/shelf_plus.dart';
-
-void main() => shelfRun(handler, defaultBindAddress: '0.0.0.0');
--- pkgs/hours/lib/hours.dart
+++
@@ -1,1 +0,0 @@
-export './src/handler.dart';
