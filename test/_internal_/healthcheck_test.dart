9dd45e1e791a183d2b1d3ad1a9a0754bb020480e
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2023-08-11 19:45:56 +0200

    test: update tests for healthcheck route

--- test/_internal_/healthcheck_test.dart
+++ test/_internal_/healthcheck_test.dart
@@ -2,25 +2,43 @@ import 'dart:io';
 
 import 'package:dart_frog/dart_frog.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 import 'package:test/test.dart';
 
 import '../../routes/_internal_/healthcheck.dart' as route;
 
 class MockRequestContext extends Mock implements RequestContext {}
 
+class MockDb extends Mock implements Db {}
+
 void main() {
+  final context = MockRequestContext();
+  final db = MockDb();
+
+  when(() => context.read<Db>()).thenReturn(db);
+
   group(
     'GET /_internal_/healthcheck',
     () {
       test(
-        'responds with healthy and status ok',
-        () async {
-          final context = MockRequestContext();
+        'returns 200 healthy',
+        () {
+          when(() => db.isConnected).thenReturn(true);
 
           final response = route.onRequest(context);
 
           expect(response.statusCode, equals(HttpStatus.ok));
-          expect(response.body(), completion(equals('Healthy')));
+        },
+      );
+
+      test(
+        'returns 503 unhealthy when database is not connected',
+        () {
+          when(() => db.isConnected).thenReturn(false);
+
+          final response = route.onRequest(context);
+
+          expect(response.statusCode, equals(HttpStatus.serviceUnavailable));
         },
       );
     },
