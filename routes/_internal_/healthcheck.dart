ce399ae17210de539fd4579e2d8027ab6e74166e
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2023-08-11 19:37:12 +0200

    feat: add database connection

--- routes/_internal_/healthcheck.dart
+++ routes/_internal_/healthcheck.dart
@@ -1,6 +1,18 @@
+import 'dart:io';
+
 import 'package:dart_frog/dart_frog.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 
 Response onRequest(RequestContext context) {
+  final db = context.read<Db>();
+
+  if (!db.isConnected) {
+    return Response(
+      body: 'No database connection',
+      statusCode: HttpStatus.serviceUnavailable,
+    );
+  }
+
   return Response(
     body: 'Healthy',
   );
---
+++ routes/_middleware.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+import 'package:mongo_dart/mongo_dart.dart';
+
+const localDbUrl = 'mongodb://localhost:2701/automatons';
+
+Handler middleware(Handler handler) {
+  final db = Db(Platform.environment['dbUrl'] ?? localDbUrl);
+
+  return (context) async {
+    final context_ = context.provide<Db>(() => db);
+
+    await db.open();
+    final response = handler(context_);
+    await db.close();
+
+    return response;
+  };
+}
