7564d5e634202adde59c4c088b4268a00559c4d2
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2023-10-08 10:26:21 +0200

    feat: add try-catch around entire app

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:automatons/domain/domain.dart';
 import 'package:automatons/repositories/user.dart';
 import 'package:dart_frog/dart_frog.dart';
@@ -6,16 +8,23 @@ Handler middleware(
   Handler handler,
 ) {
   return (context) async {
-    await db.open();
+    try {
+      await db.open();
 
-    final response = await handler //
-        .use(requestLogger())
-        .use(provider((_) => db))
-        .use(provider((_) => UserRepo(db: db)))
-        .call(context);
+      final response = await handler //
+          .use(requestLogger())
+          .use(provider((_) => db))
+          .use(provider((_) => UserRepo(db: db)))
+          .call(context);
 
-    await db.close();
+      await db.close();
 
-    return response;
+      return response;
+    } catch (e) {
+      return Response.json(
+        statusCode: HttpStatus.internalServerError,
+        body: e.toString(),
+      );
+    }
   };
 }
