9b637f7aa0bc69cee4f11a26ec2de9987922e55c
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2023-11-24 17:28:19 +0100

    fix: remove unused import

--- routes/api/session/cancel.dart
+++ routes/api/session/cancel.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:dart_frog/dart_frog.dart';
 import 'package:horahora/generated/nl_iruoy_horahora_v0_json.dart';
 import 'package:horahora/repositories/job.dart';
-import 'package:horahora/repositories/record.dart';
 import 'package:horahora/repositories/session.dart';
 
 FutureOr<Response> onRequest(
