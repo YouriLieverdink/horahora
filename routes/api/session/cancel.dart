5ee557899e158aab8ce3ab15f71461bdd4c2c055
Youri <89806453+YouriLieverdink@users.noreply.github.com>
2023-11-24 17:52:36 +0100

    feat: add ability to cancel session (#9)
    * feat: add ability to cancel session
    
    * fix: remove unused import

---
+++ routes/api/session/cancel.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+import 'package:horahora/generated/nl_iruoy_horahora_v0_json.dart';
+import 'package:horahora/repositories/job.dart';
+import 'package:horahora/repositories/session.dart';
+
+FutureOr<Response> onRequest(
+  RequestContext context,
+) async {
+  switch (context.request.method) {
+    case HttpMethod.post:
+      return _post(context);
+
+    default:
+      return Response(
+        statusCode: HttpStatus.methodNotAllowed,
+      );
+  }
+}
+
+FutureOr<Response> _post(
+  RequestContext context,
+) async {
+  final sessionRepo = context.read<SessionRepo>();
+  final jobRepo = context.read<JobRepo>();
+
+  final json = await context.request.json();
+  final form = SessionForm.fromJson(json);
+
+  final job = await jobRepo.findById(form.jobId);
+  if (job == null) {
+    return Response.json(
+      statusCode: HttpStatus.notFound,
+      body: 'Job: ${form.jobId} not found.',
+    );
+  }
+
+  final current = await sessionRepo.findByJob(form.jobId);
+  if (current == null) {
+    return Response.json(
+      statusCode: HttpStatus.conflict,
+      body: 'Session for job: ${form.jobId} not yet active.',
+    );
+  }
+
+  await sessionRepo.deleteOne(current.id);
+
+  return Response(statusCode: 204);
+}
